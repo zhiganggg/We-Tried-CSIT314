f8f3d423c98ef12e5dd2befe5d4567cb32e54a3d
zhiganggg <zhigang.ch98@gmail.com>
2024-04-07 18:49:35 +0800

    update

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = '1H{LkD*3oWf$8yU!q2RzN@7vP'
+  app.config['SQLALCHEMY_DATABSE_URI'] = f'sqlite:///{DB_NAME}'
+  db.init_app(app)
 
   from .views import views
   from .auth import auth
@@ -10,5 +17,14 @@ def create_app():
   app.register_blueprint(views, url_prefix='/')
   app.register_blueprint(auth, url_prefix='/')
 
+  from .models import User, Note
+
+  create_database(app)
+
   return app
+
+def create_database(app):
+  if not path.exists('website/' + DB_NAME):
+    db.create_all(app=app)
+    print('Created Database!')
   
