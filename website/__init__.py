88e3101c70729c2d23904dd10067dc07ea9377c9
zhiganggg <zhigang.ch98@gmail.com>
2024-04-08 10:05:09 +0800

    changes

--- website/__init__.py
+++ website/__init__.py
@@ -1,24 +1,36 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = '1H{LkD*3oWf$8yU!q2RzN@7vP'
-  app.config['SQLALCHEMY_DATABSE_URI'] = f'sqlite:///{DB_NAME}'
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
   db.init_app(app)
 
   from .views import views
   from .auth import auth
+  
   app.register_blueprint(views, url_prefix='/')
   app.register_blueprint(auth, url_prefix='/')
 
   from .models import User, Note
 
-  create_database(app)
+  with app.app_context():
+    db.create_all()
+
+  login_manager = LoginManager()
+  login_manager.login_view = 'auth.login'
+  login_manager.init_app(app)
+
+  @login_manager.user_loader
+  def load_user(id):
+    return User.query.get(int(id))
 
   return app
 
